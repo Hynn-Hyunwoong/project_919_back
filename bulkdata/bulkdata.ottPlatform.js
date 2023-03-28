1cedb7b1205814b2ac330b005e5b03e98267ff86
Hynn-Hyunwoong <choihwoong@me.com>
2023-03-29 05:40:31 +0900

    feat:PlatformLogo

--- bulkdata/bulkdata.ottPlatform.js
+++ bulkdata/bulkdata.ottPlatform.js
@@ -1,11 +1,41 @@
 const now = new Date()
 const ottPlatformData = [
-  { platformName: 'Youtube', createdAt: now, updatedAt: now },
-  { platformName: 'Netflix', createdAt: now, updatedAt: now },
-  { platformName: 'DisneyPlus', createdAt: now, updatedAt: now },
-  { platformName: 'Watcha', createdAt: now, updatedAt: now },
-  { platformName: 'Tving', createdAt: now, updatedAt: now },
-  { platformName: 'Wavve', createdAt: now, updatedAt: now },
+  {
+    platformName: 'Youtube',
+    createdAt: now,
+    updatedAt: now,
+    Image: '/public/img/YoutubeLogo.png',
+  },
+  {
+    platformName: 'Netflix',
+    createdAt: now,
+    updatedAt: now,
+    Image: '/public/img/NetflixLogo.png',
+  },
+  {
+    platformName: 'DisneyPlus',
+    createdAt: now,
+    updatedAt: now,
+    Image: '/public/img/DisneyPlusLogo.png',
+  },
+  {
+    platformName: 'Watcha',
+    createdAt: now,
+    updatedAt: now,
+    Image: '/public/img/WatchaLogo.png',
+  },
+  {
+    platformName: 'Tving',
+    createdAt: now,
+    updatedAt: now,
+    Image: '/public/img/TvingLogo.png',
+  },
+  {
+    platformName: 'Wavve',
+    createdAt: now,
+    updatedAt: now,
+    Image: '/public/img/WavveLogo.png',
+  },
 ]
 
 module.exports = ottPlatformData
